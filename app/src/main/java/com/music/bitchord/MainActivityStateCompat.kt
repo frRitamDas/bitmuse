bacf682e1d66c71993551c2de1c36c47baeafa84
frRitamDas <ritamdas20012@gmail.com>
2026-09-07 01:38:45 +0530

    Fix session mode compatibility declaration

--- app/src/main/java/com/music/bitchord/MainActivityStateCompat.kt
+++ app/src/main/java/com/music/bitchord/MainActivityStateCompat.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import com.music.bitchord.auth.WebSessionMode
 
 typealias WebSessionMode = com.music.bitchord.auth.WebSessionMode
 
